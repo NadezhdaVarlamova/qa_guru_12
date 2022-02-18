47c5e04404cb196a66f429bdccaa3a3857d2185b
varlamova <varlamvanadia@mail.ru>
2022-02-18 18:21:10 +0300

    Добавила отправку отчета в телеграм

---
+++ src/test/java/gmail/com/varlamvanadia1996/pages/StudentData.java
@@ -0,0 +1,25 @@
+package gmail.com.varlamvanadia1996.pages;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Tag;
+
+
+public class StudentData {
+    Faker faker = new Faker();
+    public final String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = "Female",
+            phone = faker.phoneNumber().subscriberNumber(10),
+            year = String.valueOf(faker.number().numberBetween(1990, 2020)),
+            month = "March",
+            day = String.valueOf(faker.number().numberBetween(1, 31)),
+            subjects = "Maths",
+            hobbiesSports = "Sports",
+            hobbiesReading = "Reading",
+            hobbiesMusic = "Music",
+            fileName = "test.txt",
+            address = faker.address().fullAddress(),
+            state = "Haryana",
+            city = "Karnal";
+}
---
+++ src/test/java/gmail/com/varlamvanadia1996/pages/components/CalendarComponent.java
@@ -0,0 +1,17 @@
+package gmail.com.varlamvanadia1996.pages.components;
+
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Tag;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CalendarComponent {
+    @Step("Выбираем дату рождения")
+    public void setDae(String year, String month, String day) {
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month-select").selectOption(month);
+        $(byText(day)).click();
+    }
+}
