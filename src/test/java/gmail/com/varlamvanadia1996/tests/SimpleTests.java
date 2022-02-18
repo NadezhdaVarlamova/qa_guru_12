66d6e35d6eebdb2e1f325170df0e4a04c594952b
varlamova <varlamvanadia@mail.ru>
2022-02-18 18:35:40 +0300

    Добавила сломанный тест

---
+++ src/test/java/gmail/com/varlamvanadia1996/tests/SimpleTests.java
@@ -0,0 +1,15 @@
+package gmail.com.varlamvanadia1996.tests;
+
+import gmail.com.varlamvanadia1996.helpers.Attach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("properties")
+public class SimpleTests {
+    @Test
+    public void SimpleTest(){
+        assertTrue(false);
+    }
+}
